85efdc4b3057e60ed5085f0ee738af9d9686396f
Rohit <rohitcharmode2590@gmail.com>
2026-04-10 05:09:46 +0530

    App5 -Implementing Continuous Conversations

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import uuid
+
 from dotenv import load_dotenv
 import os
 from datetime import datetime
@@ -6,11 +8,16 @@ from langchain_ollama import ChatOllama
 from langchain.agents import create_agent
 import gradio as gr
 
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 load_dotenv()
 
 def get_date():
     """Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
+
+conn = sqlite3.connect("chatbot_memory.db", check_same_thread=False)
+checkpointer = SqliteSaver(conn)
 
 llm = ChatOllama(model="qwen3-vl:4b")
 
@@ -20,16 +27,25 @@ Answer all user's queries.
 Use the get_date tool if the user is asking about the today's date.
 """
 
-agent = create_agent(model=llm, tools=[get_date], system_prompt=system_prompt)
+agent = create_agent(
+    model=llm,
+    tools=[get_date],
+    system_prompt=system_prompt,
+    checkpointer=checkpointer)
 
-def chat(message, history):
-    response = agent.invoke({"messages":[{"role":"user", "content": message}]})
+def chat(message, history, thread_id):
+    config = {"configurable": {"thread_id": thread_id}}
+    response = agent.invoke(
+        {"messages":[{"role":"user", "content": message}]},
+        config
+    )
     last_response = response['messages'][-1].content
     return last_response
 #print(response['messages'][-1].content)
 
 with gr.Blocks() as demo:
     gr.Markdown("# AI Chatbot")
+    thread_id = gr.State(value=lambda: str(uuid.uuid4()))
     gr.ChatInterface(fn=chat)
 
 demo.launch()
