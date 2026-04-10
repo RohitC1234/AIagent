b80ab23c61117b7d6eef0f5eb3ebfa336c63a10f
Rohit <rohitcharmode2590@gmail.com>
2026-04-10 05:43:16 +0530

    App5 -Adding Agent Web Search Capability

--- main.py
+++ main.py
@@ -10,11 +10,16 @@ import gradio as gr
 
 from langgraph.checkpoint.sqlite import SqliteSaver
 import sqlite3
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+
 load_dotenv()
 
 def get_date():
     """Get the current date"""
     return datetime.now().strftime("%Y-%m-%d")
+
+search_tool = TavilySearchResults()
 
 conn = sqlite3.connect("chatbot_memory.db", check_same_thread=False)
 checkpointer = SqliteSaver(conn)
@@ -24,12 +29,13 @@ llm = ChatOllama(model="qwen3-vl:4b")
 system_prompt = """ 
 You are a helpful assistant.
 Answer all user's queries.
+Use the get_date tool ONLY when the user is explicitly asking about the today's date.
 Use the get_date tool if the user is asking about the today's date.
 """
 
 agent = create_agent(
     model=llm,
-    tools=[get_date],
+    tools=[get_date, search_tool],
     system_prompt=system_prompt,
     checkpointer=checkpointer)
 
